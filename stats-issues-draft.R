3af48cf361852b2a836a9e01794b3c6353cfd4d2
Erick Chacon Montalvan <eral.th07@gmail.com>
2017-08-26 19:51:59 +0100

    testing menu

---
+++ stats-issues-draft.R
@@ -0,0 +1,26 @@
+# library(devtools)
+# devtools::install_github('rstudio/blogdown')
+
+library(blogdown)
+# install_theme("digitalcraftsman/hugo-material-docs",
+#              theme_example = TRUE, update_config = TRUE)
+##
+# blogdown::install_hugo()
+
+new_site('.', theme = 'digitalcraftsman/hugo-material-docs')
+
+#setwd('/Users/susanaromero/Documents/1.VR_BLOGPOSTS/BlogRPosts/statsissues')
+#getwd()
+
+# Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio/bin/pandoc")
+blogdown::serve_site()
+
+blogdown::build_site()
+
+blogdown::new_post("Likelihood", ext = '.Rmd')
+blogdown::new_post("linear-regression", ext = '.Rmd')
+blogdown::new_post("Introduction to R", ext = '.Rmd')
+blogdown::new_post("Introduction to Python", ext = '.Rmd')
+
+# we will create another repository to put other topics separately for programming
+
